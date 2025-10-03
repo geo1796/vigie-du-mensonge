4e0c6266063da9d96919f233610203d06fe5baa2
geo1796 <geoffrey.1796@gmail.com>
2025-10-03 20:32:31 +0200

    bug fix sur /sign-in (backend)

--- backend/api/routes/auth/routes/sign_in/repository.go
+++ backend/api/routes/auth/routes/sign_in/repository.go
@@ -23,7 +23,7 @@ func (r *repository) findUserByEmail(email string) (models.User, error) {
 		Preload("Roles", func(db *gorm.DB) *gorm.DB {
 			return db.Select("id", "name")
 		}).
-		Select("id", "email", "password", "tag", "created_at", "updated_at", "deleted_at").
+		Select("id", "password", "tag", "created_at", "updated_at").
 		First(&user).Error; err != nil {
 		return models.User{}, err
 	}
